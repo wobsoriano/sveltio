bfabe08a38c05770d0846fd71491836781d22dc7
Robert Soriano <sorianorobertc@gmail.com>
2022-07-05 09:44:41 -0700

    feat: allow notifyInSync option

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import { proxy, snapshot, subscribe } from 'valtio/vanilla'
 
-function useSnapshot<T extends object>(proxyObject: T) {
+interface Options {
+  sync?: boolean
+}
+
+function useSnapshot<T extends object>(
+  proxyObject: T,
+  options?: Options,
+) {
+  const notifyInSync = options?.sync
   return {
     subscribe(fn: (payload: T) => void) {
       fn(snapshot(proxyObject) as T)
       return subscribe(proxyObject, () => {
         fn(snapshot(proxyObject) as T)
-      })
+      }, notifyInSync)
     },
   }
 }
